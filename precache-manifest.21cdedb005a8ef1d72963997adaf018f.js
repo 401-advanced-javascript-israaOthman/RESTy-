818af671ca9ff69ac1ff7b3ec38dea2265ccc1bd
israaothman <runner@fv-az70.cd0oihsif5tudkhqzttjre0vnc.bx.internal.cloudapp.net>
2020-06-29 16:35:24 +0000

    Updates

---
+++ precache-manifest.21cdedb005a8ef1d72963997adaf018f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945ab100e5a81e4f064424b48efb1724",
+    "url": "/RESTy-/index.html"
+  },
+  {
+    "revision": "15e379af07aed03f4f41",
+    "url": "/RESTy-/static/css/main.9ab42bbe.chunk.css"
+  },
+  {
+    "revision": "0828aecb8d1951d6e3e4",
+    "url": "/RESTy-/static/js/2.e37d5b6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RESTy-/static/js/2.e37d5b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e379af07aed03f4f41",
+    "url": "/RESTy-/static/js/main.18949de0.chunk.js"
+  },
+  {
+    "revision": "33b42b919687db33fe19",
+    "url": "/RESTy-/static/js/runtime-main.81616f1b.js"
+  }
+]);
